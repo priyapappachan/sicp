2aeef63a8db91932c7b94225dbed81f8559480fc
priyapappachan <priyapappachan010@gmail.com>
2014-07-11 09:52:21 +0530

    interval div

---
+++ 2.10.scm
@@ -0,0 +1,21 @@
+(define (make-interval a b)
+	(cons a b))
+
+(define (mul-interval x y)
+	(let ((p1 (* (lower-bound x) (lower-bound y)))
+	        (p2 (* (lower-bound x) (upper-bound y)))
+	        (p3 (* (upper-bound x) (lower-bound y)))
+	        (p4 (* (upper-bound x) (upper-bound y))))
+	(make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (spans-zero y)
+	(and (<= (lower-bound y) 0)
+	(>= (upper-bound y) 0)))
+
+(define (div-interval x y)
+	(if (spans-zero y)
+		(display "Error")
+	(mul-interval x
+        	(make-interval (/ 1.0 (upper-bound y))
+                	(/ 1.0 (lower-bound y))))))
